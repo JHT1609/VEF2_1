1693a137abf2fdf65174650cd05c1f2a383de2ae
JHT1609 <johannesto245@nemi.tskoli.is>
2018-01-18 14:02:28 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -9,14 +9,14 @@ def index():
 
 @route('/about')
 def about():
-    return "<h2>About us</h2>"
+    return "<h2> About us </h2>"
 
 @route('/contact')
 def about():
-    return "<h2>Contact us</h2>"
+    return "<h2> Contact us </h2>"
 
 @route('/users')
 def about():
-    return "<h2>Users</h2>"
+    return "<h2> Users </h2>"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))

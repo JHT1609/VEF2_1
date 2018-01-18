2eb6c7a7600e88672ce769e28b91189389942534
JHT1609 <johannesto245@nemi.tskoli.is>
2018-01-18 13:59:21 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -3,6 +3,20 @@ from bottle import route, run
 
 @route('/')
 def index():
-    return "Bottle - Heroku deployment"
+    return "<a href='/about'>About us</a>" \
+            "<a href='/contact'>Contact</a>" \
+            "<a href='/user'>Users</a>"
+
+@route('/about')
+def about():
+    return "<h2>About us</h2>"
+
+@route('/contact')
+def about():
+    return "<h2>Contact us</h2>"
+
+@route('/users')
+def about():
+    return "<h2>Users</h2>"
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
